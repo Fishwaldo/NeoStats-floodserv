3267a3c674df0f4ebae11e2201c2029d64f0a7ad
Fish <>
2006-01-26 15:32:19 +0000

    update copyright year

--- floodserv.c
+++ floodserv.c
@@ -1,5 +1,5 @@
 /* NeoStats - IRC Statistical Services 
-** Copyright (c) 1999-2005 Adam Rutter, Justin Hammond, Mark Hetherington
+** Copyright (c) 1999-2006 Adam Rutter, Justin Hammond, Mark Hetherington
 ** http://www.neostats.net/
 **
 **  This program is free software; you can redistribute it and/or modify
@@ -110,7 +110,7 @@ static const char *fs_about[] =
 /** copyright info */
 static const char *fs_copyright[] = 
 {
-	"Copyright (c) 1999-2005, NeoStats",
+	"Copyright (c) 1999-2006, NeoStats",
 	"http://www.neostats.net/",
 	NULL
 };
